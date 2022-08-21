385b769c7eac589609ea221c45fbce289ec35a24
tianci <newanobj@foxmail.com>
2022-08-21 15:04:04 +0800

    登陆功能--登录页面的基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import styles from './index.module.scss'
 export default function Login() {
   return (
-    <div className="Login">
-      <Navbar extra="分享">首页</Navbar>
-      <Navbar extra="更多">文章</Navbar>
-      <Navbar extra="···">个人</Navbar>
+    <div className={styles.root}>
+      <Navbar>登录</Navbar>
+      <div className="content">
+        {/* 标题 */}
+        <h3>短信登录</h3>
+        <form>
+          {/* 手机号输入框 */}
+          <div className="input-item">
+            <div className="input-box">
+              <input
+                className="input"
+                name="mobile"
+                placeholder="请输入手机号"
+                autoComplete="off"
+              />
+            </div>
+            <div className="validate">手机号验证错误信息</div>
+          </div>
+
+          {/* 短信验证码输入框 */}
+          <div className="input-item">
+            <div className="input-box">
+              <input
+                className="input"
+                name="code"
+                placeholder="请输入验证码"
+                maxLength={6}
+                autoComplete="off"
+              />
+              <div className="extra">获取验证码</div>
+            </div>
+            <div className="validate">验证码验证错误信息</div>
+          </div>
+
+          {/* 登录按钮 */}
+          <button type="submit" className="login-btn">
+            登录
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
